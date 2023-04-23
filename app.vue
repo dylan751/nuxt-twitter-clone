3c19fc358b6b99b9d546a16b7681653de6f212ce
Nguyen Duong <muoi07052001@gmail.com>
2023-04-23 09:34:55 +0700

    Add Main Section

--- app.vue
+++ app.vue
@@ -13,8 +13,8 @@
           </div>
 
           <!-- Main content -->
-          <main class="col-span-12 md:col-span-8 xl:col-span-6 bg-red-500">
-            <h1>asd</h1>
+          <main class="col-span-12 md:col-span-8 xl:col-span-6">
+            <router-view />
           </main>
 
           <!-- Right sidebar -->
