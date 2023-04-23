9cded1812f540335b1ef2cf9045cbcc2c584c38d
Nguyen Duong <muoi07052001@gmail.com>
2023-04-23 09:19:23 +0700

    Add Right Sidebar

--- app.vue
+++ app.vue
@@ -6,13 +6,23 @@
           class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5"
         >
           <!-- Left sidebar -->
-          <div class="md:block xs-col-span-1 xl-col-span-2">
-            <div class="sticky top-0"><SidebarLeft /></div>
+          <div class="hidden md:block xs-col-span-1 xl:col-span-2">
+            <div class="sticky top-0">
+              <SidebarLeft />
+            </div>
           </div>
 
           <!-- Main content -->
+          <main class="col-span-12 md:col-span-8 xl:col-span-6 bg-red-500">
+            <h1>asd</h1>
+          </main>
 
           <!-- Right sidebar -->
+          <div class="hidden col-span-12 md:block xl:col-span-4 md:col-span-3">
+            <div class="sticky top-0">
+              <SidebarRight />
+            </div>
+          </div>
         </div>
       </div>
     </div>
---
+++ components/Sidebar/Right/index.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="flex flex-col">
+    <!-- Preview Card: What's happening -->
+    <SidebarRightPreviewCard title="What's happening">
+      <SidebarRightPreviewCardItem
+        v-for="whatsHappening in whatsHappeningItems"
+        :key="whatsHappening.title"
+      >
+        <div>
+          <h2 class="font-bold text-gray-800 text-md dark:text-white">
+            {{ whatsHappening.title }}
+          </h2>
+
+          <p class="text-xs text-gray-400">{{ whatsHappening.count }}</p>
+        </div>
+      </SidebarRightPreviewCardItem>
+    </SidebarRightPreviewCard>
+
+    <!-- Preview Card: Who to follow -->
+    <SidebarRightPreviewCard title="Who to follow">
+      <SidebarRightPreviewCardItem
+        v-for="whoToFollow in whoToFollowItems"
+        :key="whoToFollow.handle"
+      >
+        <div class="flex flex-row justify-between p-2 items-center">
+          <div class="flex flex-row">
+            <img
+              class="w-10 h-10 rounded-full"
+              :src="whoToFollow.image"
+              :alt="whoToFollow.name"
+            />
+            <div class="flex flex-col ml-2">
+              <h1 class="text-sm font-bold text-gray-900 dark:text-white">
+                {{ whoToFollow.name }}
+              </h1>
+              <p class="text-xs text-gray-400">{{ whoToFollow.handle }}</p>
+            </div>
+          </div>
+          <div class="flex h-full">
+            <button
+              class="px-4 py-2 font-bold text-xs text-white dark:text-black bg-black dark:bg-white rounded-full"
+            >
+              Follow
+            </button>
+          </div>
+        </div>
+      </SidebarRightPreviewCardItem>
+    </SidebarRightPreviewCard>
+  </div>
+</template>
+<script setup>
+const whatsHappeningItems = ref([
+  {
+    title: "SpaceX",
+    count: "18.8k Tweets",
+  },
+  {
+    title: "#CodingIsFun",
+    count: "8.8k Tweets",
+  },
+  {
+    title: "#artforall",
+    count: "1.8k Tweets",
+  },
+]);
+
+const whoToFollowItems = ref([
+  {
+    name: "Joe Biden",
+    handle: "@JoeBiden",
+    image: "https://picsum.photos/200/200",
+  },
+  {
+    name: "Joe Biden",
+    handle: "@JoeBiden",
+    image: "https://picsum.photos/200/200",
+  },
+  {
+    name: "Joe Biden",
+    handle: "@JoeBiden",
+    image: "https://picsum.photos/200/200",
+  },
+]);
+</script>
