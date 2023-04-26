6a69725ae72c102b4bba75cc42e0fcac0cd020ee
Nguyen Duong <muoi07052001@gmail.com>
2023-04-26 20:59:01 +0700

    Upload images to Cloudinary

--- server/api/user/tweets/index.post.js
+++ server/api/user/tweets/index.post.js
@@ -2,6 +2,7 @@ import formidable from "formidable";
 import { createTweet } from "~/server/db/tweets";
 import { tweetTransformer } from "~/server/transformers/tweet";
 import { createMediaFile } from "~/server/db/mediaFiles";
+import { uploadToCloudinary } from "~/server/utils/cloudinary";
 
 export default defineEventHandler(async (event) => {
   const form = formidable({});
@@ -27,9 +28,13 @@ export default defineEventHandler(async (event) => {
   const tweet = await createTweet(tweetData);
 
   const filePromises = Object.keys(files).map(async (key) => {
+    const file = files[key];
+
+    const cloudinaryResource = await uploadToCloudinary(file.filepath);
+
     return createMediaFile({
-      url: "",
-      providerPublicId: "random_id",
+      url: cloudinaryResource.secure_url,
+      providerPublicId: cloudinaryResource.public_id,
       userId: userId,
       tweetId: tweet.id,
     });
@@ -39,5 +44,6 @@ export default defineEventHandler(async (event) => {
 
   return {
     tweet: tweetTransformer(tweet),
+    files,
   };
 });
