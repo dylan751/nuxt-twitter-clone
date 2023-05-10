9dbaa28a43e618e4e5e50b7fe654173dffa0e1f6
Nguyen Duong <muoi07052001@gmail.com>
2023-05-10 11:35:27 +0700

    Add API for tweets

--- server/transformers/tweet.js
+++ server/transformers/tweet.js
@@ -1,6 +1,16 @@
+import { mediaFileTransformer } from "./mediaFiles";
+import { userTransformer } from "./user";
+
 export const tweetTransformer = (tweet) => {
   return {
     id: tweet.id,
     text: tweet.text,
+    mediaFiles: tweet.mediaFiles
+      ? tweet.mediaFiles.map(mediaFileTransformer)
+      : [],
+    author: tweet.author ? userTransformer(tweet.author) : null,
+    replies: tweet.replies ? tweet.replies.map(tweetTransformer) : [],
+    replyTo: tweet.replyTo ? tweetTransformer(tweet.replyTo) : null,
+    repliesCount: tweet.replies ? tweet.replies.length : 0,
   };
 };
