858ad6c390b00aec31f52883ccfeb98f6c57dc1f
Nguyen Duong <muoi07052001@gmail.com>
2023-05-17 22:44:26 +0700

    Add icons

--- components/Tweet/Item/Actions/index.vue
+++ components/Tweet/Item/Actions/index.vue
@@ -1,8 +1,32 @@
 <template>
-  <div>
+  <div class="flex items-center justify-around w-full">
     <TweetItemActionsIcon color="blue">
       <template v-slot:icon="{ classes }">
         <ChatIcon :class="classes" />
+      </template>
+
+      <template v-slot:default> 5 </template>
+    </TweetItemActionsIcon>
+
+    <TweetItemActionsIcon color="green">
+      <template v-slot:icon="{ classes }">
+        <RefreshIcon :class="classes" />
+      </template>
+
+      <template v-slot:default> 5 </template>
+    </TweetItemActionsIcon>
+
+    <TweetItemActionsIcon color="red">
+      <template v-slot:icon="{ classes }">
+        <HeartIcon :class="classes" />
+      </template>
+
+      <template v-slot:default> 5 </template>
+    </TweetItemActionsIcon>
+
+    <TweetItemActionsIcon color="blue">
+      <template v-slot:icon="{ classes }">
+        <UploadIcon :class="classes" />
       </template>
 
       <template v-slot:default> 5 </template>
@@ -11,5 +35,10 @@
 </template>
 
 <script setup>
-import { ChatIcon } from "@heroicons/vue/solid";
+import {
+  ChatIcon,
+  RefreshIcon,
+  HeartIcon,
+  UploadIcon,
+} from "@heroicons/vue/outline";
 </script>
