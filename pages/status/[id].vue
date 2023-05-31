20cb51aa6f6cd18b2c6438ed3e7a8c83a2a66eaf
Nguyen Duong <muoi07052001@gmail.com>
2023-05-31 20:55:32 +0700

    API get 1 tweet

---
+++ pages/status/[id].vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <MainSection title="Tweet" :loading="loading">
+      <Head>
+        <Title></Title>
+      </Head>
+    </MainSection>
+  </div>
+</template>
+
+<script setup>
+const loading = ref(false);
+</script>
